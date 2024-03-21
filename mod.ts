2bbb7af743fcb3a778dfcfd3edd954d5dc10fc57
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2024-03-21 23:45:33 +0530

    publish readme file

--- mod.ts
+++ mod.ts
@@ -1,7 +1,15 @@
 /**
+ * @module
  * A module providing date utility functions.
  *
- * @module
+ * @example
+ * ```ts
+ * isIsoDate("2022-12-27T07:40:25.551Z");
+ * // => true
+ *
+ * isIsoDate("25/12/2022");
+ * // => false
+ * ```
  */
 
 export * from "./src/date.ts";
